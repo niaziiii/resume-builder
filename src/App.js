a37bbfa24eb25960a4d231c8265a1ebff43f62ca
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-08-15 09:54:29 -0700

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import NavigationBar from './components/NavigationBar/NavigationBar'
 import './style/style.css'
 function App() {
   return (
     <div>
+      <NavigationBar/>
     </div>
   )
 }
