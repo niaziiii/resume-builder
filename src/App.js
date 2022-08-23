30b84333bd1945ca6469b938e364dc8f13c8ce71
Muhabat Khan Niazi <muhabatkhanx@gmail.com>
2022-08-23 02:56:29 -0700

    added router to our routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React from 'react'
 import NavigationBar from './components/NavigationBar/NavigationBar'
 import UserInfo from './components/UserInfo/UserInfo'
 import './style/style.css'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './components/Forms/LoginForm/Login'
+
+
 function App() {
   return (
-    <div>
-      <NavigationBar/>
-      <div className='container-main'>
-      <UserInfo/>
+    <Router>
+      <div>
+        <NavigationBar />
+        <Switch>
+          <Route exact path='/' >
+            {<div>hey</div>}
+          </Route>
+          <Route path='/login'>
+            <Login/>
+          </Route>
+
+          <Route path='/resume'>
+            {< div className='container-main'>
+              <UserInfo />
+            </div>}
+          </Route>
+
+        </Switch>
+
       </div>
-    </div>
+    </Router >
   )
 }
 
